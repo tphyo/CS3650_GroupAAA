ada6a16acae8f6f8d108e34b6de7b6b4dee300a2
lyhoangrivera <80252641+lyhoangrivera@users.noreply.github.com>
2021-12-04 09:00:51 -0800

    Add files via upload

--- MUX.vhd
+++ MUX.vhd
@@ -1,1 +1,16 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+ENTITY MUX IS
+	PORT (
+		a, b  : IN std_logic_vector(32 DOWNTO 0); --input a, b
+		s  : IN STD_logic; --control signal s
 
+		y  : OUT STD_logic_vector(32 DOWNTO 0) --output y
+	);
+END MUX;
+ARCHITECTURE Behavioral OF MUX IS
+BEGIN
+	--when s = 0, y is the same as a
+	--when s = 1, y is the same as b
+	y <= a WHEN s = '0' ELSE b;
+END Behavioral;
